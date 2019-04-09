0347e1c2ba91bf8dad784ba17e88a335480a7b52
MaxutaM <49364236+MaxutaM@users.noreply.github.com>
2019-04-09 19:52:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-var money = prompt("Ваш бюджет на месяц?", "30000 руб"),
-	time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-
-var expenses = prompt("Введите обязательную статью расходов в этом месяце",''),
-	 HowMuch = prompt("Во скольок обойдется?",'');
-var appData = {
+let appData = {
 	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
 	timeData: time,
-	expenses: {
-	"expenses" : "HowMuch"},
-	optionalExpenses:{
-		
-	},
-	income:[],
-	saving: false
+	savings: false
 };
-var fullBudget = expenses / HowMuch;
-alert(fullBudget);
+
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	    b = prompt("Во сколько обойдется?", ''),
+
+	if(typeof(a)=== 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" &&  a.length < 50 ){
+
+		alert("Done");
+
+		appData.expenses[a] = b;
+	} else {
+		alert("Error");
+		i--;
+	}
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ваш ежедневный бюджет: " + appData.moneyPerDay + " руб");
+
+switch (appData.moneyPerDay) {
+	case appData.moneyPerDay < 100:
+		alert("У вас низкий уровень достатка");
+		break;
+	case appData.moneyPerDay > 100 && appData.moneyPerDay < 2000:
+		alert("У вас cредний уровень достатка");
+		break;
+	case appData.moneyPerDay > 2000:
+		alert("У вас высокий уровень достатка");
+		break;
+	default:
+		alert("Произошла неизвестная ошибка")
+		break;
+}
